10e79c3068a9c80956584538824820520aa381d7
Marco Tan <marco.tan.200405@gmail.com>
2021-04-16 19:26:38 -0400

    add kalman filter

---
+++ include/argon/control/Kalman.hpp
@@ -0,0 +1,23 @@
+//* ArgonLib
+//* Kalman filter utility header
+#pragma once
+#include "api.h"
+
+namespace argon {
+    class KalmanFilter {
+        public:
+            explicit KalmanFilter(double p_R, double p_Q);
+            ~KalmanFilter();
+
+            double filter(double p_U);
+
+        private:
+            const double m_R; // noise covariance
+            const double m_H {1.00}; // measurement map scalar (we assume 1 dimensional kalman)
+            double m_Q;   // covariance estimate
+            double m_P {0.0}; // error covariance estimate
+            double m_U_hat {0.0}; // estimated state
+            double m_K {0.0}; // Kalman gain
+
+    };
+}
---
+++ src/argon/control/Kalman.cpp
@@ -0,0 +1,23 @@
+//* ArgonLib
+//* Kalman filter utility source
+#include "argon/control/Kalman.hpp"
+
+namespace argon {
+    KalmanFilter::KalmanFilter(double p_R, double p_Q) : m_R{p_R}, m_Q{p_Q} {
+        std::cout << "[CTRL] Kalman created: " << pros::millis() << " ms.\n";
+    }
+
+    KalmanFilter::~KalmanFilter() {
+        std::cout << "[CTRL] Kalman destroyed: " << pros::millis() << " ms.\n";
+    }
+
+    double KalmanFilter::filter(double p_U) {
+        //? calculate Kalman gain
+        m_K = m_P * m_H / (m_H * m_P * m_H + m_R);
+        //? update estimate
+        m_U_hat += m_K * (p_U - m_H * m_U_hat);
+        //? update error covariance
+        m_P = (1 - m_K * m_H) * m_P + m_Q;
+        return m_U_hat;
+    }
+}
