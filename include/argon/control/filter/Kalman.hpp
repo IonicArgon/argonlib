751747185a5881362fc53d28b810d02256c4654c
Marco Tan <marco.tan.200405@gmail.com>
2021-04-17 19:13:49 -0400

    kalman class

---
+++ include/argon/control/filter/Kalman.hpp
@@ -0,0 +1,26 @@
+//* ArgonLib
+//* Kalman filter utility header
+#pragma once
+#include "argon/control/filter/AbstractFilter.hpp"
+
+namespace argon {
+    class KalmanFilter : public AbstractFilter {
+        public:
+            KalmanFilter(double p_R, double p_def_Q);
+            virtual ~KalmanFilter();
+
+            virtual void reset();
+            virtual double filter(double p_U);
+            KalmanFilter& set_Q(double p_Q);
+
+        private:
+            const double m_R;   // noise covariance
+            const double m_H {1.0}; // meas. map scalar
+            const double m_def_Q; // default covariance estimate
+            double m_Q; // covariance estimate
+            double m_P {0.0};   // error covariance estimate
+            double m_U_hat {0.0};   // estimated state
+            double m_K {0.0};   // Kalman gain
+
+    };
+}
---
+++ src/argon/control/filter/Kalman.cpp
@@ -0,0 +1,36 @@
+//* ArgonLib
+//* Kalman filter utility source
+#include "argon/control/filter/Kalman.hpp"
+
+namespace argon {
+    KalmanFilter::KalmanFilter(double p_R, double p_def_Q) : m_R{p_R}, m_def_Q{p_def_Q} {
+        reset();
+        std::cout << "[CTRL] KalmanFilter created: " << pros::millis() << " ms.\n";
+    }
+
+    KalmanFilter::~KalmanFilter() {
+        std::cout << "[CTRL] KalmanFilter destroyed: " << pros::millis() << " ms.\n";
+    }
+
+    void KalmanFilter::reset() {
+        m_Q = m_def_Q;
+        m_P = 0.0;
+        m_U_hat = 0.0;
+        m_K = 0.0;
+    }
+
+    double KalmanFilter::filter(double p_U) {
+        //? calculate Kalman gain
+        m_K = m_P * m_H / (m_H * m_P * m_H + m_R);
+        //? update estimate
+        m_U_hat += m_K * (p_U - m_H * m_U_hat);
+        //? update error covariance
+        m_P = (1 - m_K * m_H) * m_P + m_Q;
+        return m_U_hat;
+    }
+
+    KalmanFilter& KalmanFilter::set_Q(double p_Q) {
+        m_Q = p_Q;
+        return *this;
+    }
+}
