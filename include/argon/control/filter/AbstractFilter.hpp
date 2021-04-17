5b84f52e4decc3aba10dbb8186ec8b83dd69e9d9
Marco Tan <marco.tan.200405@gmail.com>
2021-04-17 19:13:32 -0400

    filter abstract class

---
+++ include/argon/control/filter/AbstractFilter.hpp
@@ -0,0 +1,15 @@
+//* ArgonLib
+//* AbstractFilter utility header
+#pragma once
+#include "api.h"
+
+namespace argon {
+    class AbstractFilter {
+        public:
+            AbstractFilter();
+            virtual ~AbstractFilter();
+
+            virtual void reset() = 0;
+            virtual double filter(double p_U) = 0;
+    };
+}
---
+++ src/argon/control/filter/AbstractFilter.cpp
@@ -0,0 +1,8 @@
+//* ArgonLib
+//* AbstractFilter utility source
+#include "argon/control/filter/AbstractFilter.hpp"
+
+namespace argon {
+    AbstractFilter::AbstractFilter() = default;
+    AbstractFilter::~AbstractFilter() = default;
+}
